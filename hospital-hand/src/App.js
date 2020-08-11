ecccd6c5e4a1708bdd13eaffa08487845712db8e
Prerit Bhandari <patrioticprerit@gmail.com>
2020-08-11 10:56:32 +0545

    [Added] hospital-hand

---
+++ hospital-hand/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import 'semantic-ui-css/semantic.min.css'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        gugiukhkuhkuhuk
+      </div>
+    )
+  }
+}
